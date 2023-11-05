949e534c53d33f0528c6c2cd750b6e384076e69d
DouglasB <douglas_borges09@hmail.com>
2023-11-05 19:14:30 -0300

    feat:add middleware auth user

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -2,19 +2,11 @@
 import { MenuAdmin } from "@/components/aside/menuAdmin";
 import { CardMenu } from "@/components/card/cardMenu";
 
-import { useRouter } from "next/navigation";
-import { useEffect } from "react";
 import { useUserStore } from "../store/store";
 
 export default function Home() {
   const { user } = useUserStore();
-  const router = useRouter();
-  console.log(user);
-  useEffect(() => {
-    if (!user.username) {
-      router.push("/login");
-    }
-  }, []);
+  console.log(user, " hommeeeee ");
   return (
     <>
       <MenuAdmin />
