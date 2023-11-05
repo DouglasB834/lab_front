f45b603de8bbee2d2e0499ea3a60c69f4eb8f6a4
DouglasB <douglas_borges09@hmail.com>
2023-11-05 20:38:00 -0300

    feat:add auth login page

--- src/middleware.ts
+++ src/middleware.ts
@@ -7,9 +7,13 @@ export default function middleware(req: NextRequest) {
   if (!userName) {
     return NextResponse.redirect(new URL("/login", req.url));
   }
+
+  if (req.nextUrl.pathname === "/login" && userName) {
+    return NextResponse.redirect(new URL("/", req.url));
+  }
 }
 
 export const config = {
   middleware: "blocking",
-  matcher: ["/", "/cadastrar"],
+  matcher: ["/", "/cadastrar", "/login"],
 };
